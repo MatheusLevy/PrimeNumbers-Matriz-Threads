e1532e7e525388b92aee1f8b3bcc0ff643d6b572
Matheus Levy <levybessa2001@hotmail.com>
2021-03-29 18:25:01 -0300

    Verifica Primo

--- main.c
+++ main.c
@@ -2,9 +2,73 @@
 #include <stdlib.h>
 #include <math.h>
 
-// int posi = i*n+j;
+#define TRUE 1
+#define FALSE 0
 
-void PreencherMatrizPadrao(int* matriz, int m, int n){
+#define QThreads 2
+#define m 5
+#define n 6
+
+int TotalPrimos = 0;
+
+// int posi = i*n+j;
+void PreencherMatrizPadrao(int* matriz);
+void ExibirMatriz(int* matriz);
+void SubMatriz(int* matriz, int inicio, int fim);
+int EhPrimo(int number);
+
+int main(void){
+
+    int TotalElms = n*m;
+    int BPT = floor(m/QThreads); //Blocos por thread
+    int resto = m % QThreads;
+
+    int *matriz = (int *)malloc(sizeof(int)*TotalElms);
+    PreencherMatrizPadrao(matriz);
+    ExibirMatriz(matriz);
+    int inicio = 0;
+    int fim = n-1;
+    int i;
+    int k;
+    
+    printf("BPT: %d", BPT);
+
+    printf("\n------Thread 1------");
+    for(i=0;i<BPT;i++){
+        SubMatriz(matriz,inicio,fim);
+        inicio=fim+1;
+        fim+=n;
+    }
+
+    printf("\n------Thread 2------");
+    for(i=0;i<BPT;i++){
+        SubMatriz(matriz,inicio,fim);
+        inicio=fim+1;
+        fim+=n;
+    }   
+
+    while(resto > 0){
+        printf("\n------Thread 1------");
+        SubMatriz(matriz,inicio,fim);
+        inicio = fim+1;
+        fim+=n;
+        resto--;
+
+        if(resto>0){
+            printf("\n------Thread 2------");
+            SubMatriz(matriz,inicio,fim);
+            fim+=n;
+            resto--;
+        }
+    }
+
+    printf("\nExistem %d Primos", TotalPrimos);
+    return 0;
+}
+
+
+
+void PreencherMatrizPadrao(int* matriz){
     int i;
     int j;
     for(i=0;i<m;i++){
@@ -14,7 +78,8 @@ void PreencherMatrizPadrao(int* matriz, int m, int n){
     }
 }
 
-void ExibirMatriz(int* matriz, int m, int n){
+
+void ExibirMatriz(int* matriz){
     int i;
     int j;
     for(i=0;i<m;i++){
@@ -26,81 +91,33 @@ void ExibirMatriz(int* matriz, int m, int n){
     }
 }
 
-
 void SubMatriz(int* matriz, int inicio, int fim){
     int i;
     printf("\n-----Sub MAtriz ------\n");
     for(i = inicio;i<=fim;i++){
         printf("%d \t", matriz[i]);
+        if(EhPrimo(matriz[i])){
+            TotalPrimos++;
+        }
+            
+
     }
    printf("\n");
 }
 
-void Pthread(int *matriz, int inicio, int fim);
-
-int main(void){
-
-    int m = 5;
-    int n = 5;
-
-    int TotalElms = n*m;
-    int QThreads = 2;
-    int BPT = floor(n/QThreads); //Blocos por thread
-    int resto = n % QThreads;
-
-    int *matriz = (int *)malloc(sizeof(int)*TotalElms);
-    PreencherMatrizPadrao(matriz,m,n);
-    ExibirMatriz(matriz,m,n);
-    int inicio = 0;
-    int fim = n-1;
+int EhPrimo(int number){
+    int divisores=0;
     int i;
-    int k;
-    
-    printf("BPT: %d", BPT);
-
-    for(k=0;k<QThreads;k++){
-
-        for(i=0;i<BPT;i++){
-            Join1(matriz,inicio,fim);
-            inicio=fim+1;
-            fim+=n;
+    for (i = 2; i < number; i++)
+    {
+        if(number%i == 0){
+            divisores++;
+            break;
         }
-
-        for(i=0;i<BPT;i++){
-            Join2(matriz,inicio,fim);
-            inicio=fim+1;
-            fim+=n;
-        }   
-
-        while(resto > 0){
-            Join1(matriz,inicio,fim);
-            inicio = fim+1;
-            fim+=n;
-            resto--;
-
-            if(resto>0){
-                Join2(matriz,inicio,fim);
-                fim+=n;
-                resto--;
-            }
-        } 
-
-
-        
     }
+    if(divisores == 0 && number != 0 && number != 1)
+        return TRUE;
+    else
+        return FALSE;
     
-
-    /*
-    for(i=0;i<QThreads;inicio=fim+1, fim+=TPE, i++){
-        if(resto>0){
-            SubMatriz(matriz,inicio,++fim);
-            resto--;
-        }else{
-            SubMatriz(matriz,inicio,fim);
-        }   
-    }
-    */
-
-
-    return 0;
 }
