e3de9cd445addc0af429c6576d4683922bec6262
Matheus Levy <levybessa2001@hotmail.com>
2021-03-31 13:50:43 -0300

    Finalizado Srand

--- main.c
+++ main.c
@@ -7,79 +7,90 @@
 
 #define QThreads 2
 #define m 5
-#define n 6
+#define n 5
+#define RAND_MAX 29999
+#define seed 12345
 
+int matriz[n*m];
+int resto;
+int BPT;
+int inicio;
+int fim;
 int TotalPrimos = 0;
 
+
 // int posi = i*n+j;
-void PreencherMatrizPadrao(int* matriz);
-void ExibirMatriz(int* matriz);
-void SubMatriz(int* matriz, int inicio, int fim);
+void PreencherMatrizPadrao();
+void ExibirMatriz();
+void SubMatriz(int inicio, int fim);
 int EhPrimo(int number);
+
+
 
 int main(void){
 
-    int TotalElms = n*m;
-    int BPT = floor(m/QThreads); //Blocos por thread
-    int resto = m % QThreads;
+    //int TotalElms = n*m;
+    BPT = floor(m/QThreads); //Blocos por thread
+    resto = m % QThreads;
 
-    int *matriz = (int *)malloc(sizeof(int)*TotalElms);
     PreencherMatrizPadrao(matriz);
     ExibirMatriz(matriz);
-    int inicio = 0;
-    int fim = n-1;
-    int i;
-    int k;
-    
+    inicio = 0;
+    fim = n-1;
     printf("BPT: %d", BPT);
+    ProcessarMatriz();
+   
+    return 0;
+}
 
-    printf("\n------Thread 1------");
+void ProcessarMatriz(){
+     int i;
+
+   // printf("\n------Thread 1------");
     for(i=0;i<BPT;i++){
-        SubMatriz(matriz,inicio,fim);
+        SubMatriz(inicio,fim);
         inicio=fim+1;
         fim+=n;
     }
 
-    printf("\n------Thread 2------");
+   // printf("\n------Thread 2------");
     for(i=0;i<BPT;i++){
-        SubMatriz(matriz,inicio,fim);
+        SubMatriz(inicio,fim);
         inicio=fim+1;
         fim+=n;
     }   
 
     while(resto > 0){
-        printf("\n------Thread 1------");
-        SubMatriz(matriz,inicio,fim);
+      //  printf("\n------Thread 1------");
+        SubMatriz(inicio,fim);
         inicio = fim+1;
         fim+=n;
         resto--;
 
         if(resto>0){
-            printf("\n------Thread 2------");
-            SubMatriz(matriz,inicio,fim);
+        //    printf("\n------Thread 2------");
+            SubMatriz(inicio,fim);
             fim+=n;
             resto--;
         }
     }
 
     printf("\nExistem %d Primos", TotalPrimos);
-    return 0;
 }
 
-
-
-void PreencherMatrizPadrao(int* matriz){
+void PreencherMatrizPadrao(){
     int i;
     int j;
+    srand(seed);
     for(i=0;i<m;i++){
         for(j = 0; j<n;j++){
-            matriz[i*n+j] = i*n+j; 
+            matriz[i*n+j] = rand() % RAND_MAX; 
         }
     }
 }
 
 
-void ExibirMatriz(int* matriz){
+void ExibirMatriz(){
     int i;
     int j;
     for(i=0;i<m;i++){
@@ -91,7 +102,7 @@ void ExibirMatriz(int* matriz){
     }
 }
 
-void SubMatriz(int* matriz, int inicio, int fim){
+void SubMatriz(int inicio, int fim){
     int i;
     printf("\n-----Sub MAtriz ------\n");
     for(i = inicio;i<=fim;i++){
